1785daa119015ea6669db842020d80985925bc39
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-24 02:55:38 +0430

    Done with it

--- tfa_python_sdk/tfa_python_sdk.py
+++ tfa_python_sdk/tfa_python_sdk.py
@@ -1,6 +1,8 @@
+import requests as req
+
 class TFA:
     def __init__(self, access_token) -> None:
         self.access_token = access_token
 
     def authUser(self, user_token):
-        return {'aid': self.access_token, 'uid': user_token}
+        return req.get(f'https://tele-fa-api.herokuapp.com/api/access/{self.access_token}/{user_token}').json()
