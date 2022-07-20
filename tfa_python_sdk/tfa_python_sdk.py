c4aee5daec4e2e625e3559c73d94a94a4c7ad7c8
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-20 22:48:53 +0430

    Update SDK

--- tfa_python_sdk/tfa_python_sdk.py
+++ tfa_python_sdk/tfa_python_sdk.py
@@ -5,4 +5,4 @@ class TFA:
         self.access_token = access_token
 
     def authUser(self, user_token):
-        return req.get(f'https://tele-fa-api.herokuapp.com/api/access/{self.access_token}/{user_token}').json()
+        return req.get(f'https://tele-fa-api.herokuapp.com/api/access/{self.access_token}/{user_token}')
