c626270a7eb42cedc3d0407949185834cc9a2a35
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-20 22:48:46 +0430

    Testing

--- test.py
+++ test.py
@@ -3,12 +3,3 @@ from tfa_python_sdk import TFA
 auth = TFA('access_token')
 
 result = auth.authUser('user_token')
-
-statCode = result.error
-
-if (statCode == 800):
-    print('Authenticated.')
-elif (statCode == 820):
-    print('User token is wrong.')
-elif (statCode == 290):
-    print('Admin token is wrong.')
