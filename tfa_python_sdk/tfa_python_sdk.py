42c4a19ef77f01d67605bcf8e57f6b59c7482db0
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 10:15:26 +0430

    Add return data in a dictionary

--- tfa_python_sdk/tfa_python_sdk.py
+++ tfa_python_sdk/tfa_python_sdk.py
@@ -5,4 +5,11 @@ class TFA:
         self.access_token = access_token
 
     def authUser(self, user_token):
-        return req.get(f'https://tele-fa-api.herokuapp.com/api/access/{self.access_token}/{user_token}')
+        response = req.get(f'https://tele-fa-api.herokuapp.com/api/access/{self.access_token}/{user_token}')
+
+        data = {
+            "status": response.status_code,
+            "data": response.json()
+        }
+
+        return data
