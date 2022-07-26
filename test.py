08d3729ba7bc51d2edd35da3dcda119e4f633a4c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 10:15:37 +0430

    Testing lib

--- test.py
+++ test.py
@@ -3,3 +3,12 @@ from tfa_python_sdk import TFA
 auth = TFA('access_token')
 
 result = auth.authUser('user_token')
+
+statCode = result.status
+data = result.data
+
+if (statCode == 200):
+    print('Authenticated.')
+    user = data['user']
+else:
+    print(data['message'])
