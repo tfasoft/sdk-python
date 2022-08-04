f88d44f1c15f8022e4606bec1cd76747df6a7d88
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 21:44:28 +0430

    Update testing

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ auth = TFA('access_token')
 
 result = auth.authUser('user_token')
 
-statCode = result.status
-data = result.data
+statCode = result['status']
+data = result['data']
 
 if (statCode == 200):
     print('Authenticated.')
