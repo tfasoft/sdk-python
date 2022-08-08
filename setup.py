a829177b421e56947ba7626e0b0d933e60812922
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 20:15:31 +0430

    Change the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='tfa-python-sdk',
-    version='1.5.0',
+    version='1.6.0',
     author='Amirhossein Mohammadi',
     license='MIT',
     author_email="amirhosseinmohammadi1380@yahoo.com",
